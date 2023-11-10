6946f5544ac543e8868809f6b80564bd1e1562ea
N0fish <134721970+N0fish@users.noreply.github.com>
2023-11-10 11:50:22 +0100

    mod_09_nov

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: algultse <algultse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 15:26:16 by algultse          #+#    #+#             */
-/*   Updated: 2023/11/09 15:28:34 by algultse         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:55:21 by algultse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,35 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	size_t	i;
 	
+	if (!big && !len)
+		return (0);
+	if (!*little)
+		return ((char *)big);
+	while (len-- && *big)
+	{
+		i = 0;
+		while ((*(big + i) == *(little + i)) && *(little + i) && len >= i)
+		{
+			if (!*(little + i + 1))
+				return ((char *)big);
+			i++;
+		}
+		big++;
+	}
+	return  (NULL);
 }
 
+/*
 #include <stdio.h>
 int	main()
 {
-	strnstr();
+	char str1[] = "Test string";
+	char str2[] = "str";
+	
+	printf("ft_strnstr: [%s]\n", ft_strnstr(str1, str2, (size_t)8));
+	printf("strnstr: [%s]\n", strnstr(str1, str2, (size_t)8));
 	return (0);
 }
+*/
