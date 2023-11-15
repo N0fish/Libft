6c1951e158a53f7f93b7c622aff6128c86a9599a
N0fish <134721970+N0fish@users.noreply.github.com>
2023-11-15 10:31:39 +0100

    mon_15_nov

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: algultse <algultse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:14:38 by algultse          #+#    #+#             */
-/*   Updated: 2023/11/14 16:15:59 by algultse         ###   ########.fr       */
+/*   Updated: 2023/11/15 10:30:12 by algultse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,31 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
+	}
+	if (n < 0)
+	{	
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n + '0', fd);
+		return ;
+	}
+	ft_putnbr_fd(n / 10, fd);
+	ft_putnbr_fd(n % 10, fd);
 }
 
 /*
 #include <stdio.h>
 int main()
 {
-	ft_putnbr_fd(32, 0);
+	ft_putnbr_fd(-650, 0);
 	return (0);
 }
 */
